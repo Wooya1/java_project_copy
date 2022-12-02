ca8a0fea57bf1ac1395ad03c09570753fea7bd61
Wooya1 <zlddhvk12@gmail.com>
2022-12-02 15:36:40 +0900

    20221202수업1

--- java_project/src/ch05_test_1202/PersonTest.java
+++ java_project/src/ch05_test_1202/PersonTest.java
@@ -1,13 +1,18 @@
-package constructor;
+package ch05_test_1202;
 
 public class PersonTest {
    
 	 public static void main(String[] args) {
 		  Person lsy = new Person();
-		  lsy.name = "조민우";
+//		  lsy.name = "조민우";
 		  lsy.height = 176F;
 		  lsy.weight = 102F;
+		  lsy.setName("조민우세터로추가했음.");
 		  lsy.shwoInfo();
+		  System.out.println("get메서드로 이름호출"
+				  + lsy.getName());
+		  //set 를 이용해서 name 값 설정하기.
+		 
 		  
 		  Person lsy2 = new Person("조민우");
 		  lsy.height = 176F;
